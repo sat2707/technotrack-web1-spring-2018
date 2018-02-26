7f24ac03f5142c2a34d1d54e6065a700d609d56a
Elisey <qqq@qqq.com>
2018-02-26 13:37:54 +0300

    1i1i1i1i1i1i1i1i1i1i1i1i

--- lesson1/server/run.py
+++ lesson1/server/run.py
@@ -1,30 +1,30 @@
 # -*- coding: utf-8 -*-
 import socket
-from os import listdir
+from os import listdir #for list of files
 
 
 
 def get_response(request):
-	top = request.split('\n')
-	avm = top[0].split()[1]
-	if avm == '/':
+	top = request.split('\n') 												#spliting by \n
+	avm = top[0].split()[1] 												#getting address
+	if avm=='/':
 		for i in range(len(top)):
-			if ''.join(top[i][:11]) =='User-Agent:':
+			if ''.join(top[i][:11])=='User-Agent:': 						#check for eq
 				k=i
-		agent = ' '.join(top[k].split()[1:])
-		return 'HTTP/1.1 200 OK\n\n Hello mister! \n You are: ' + agent
-	elif avm == '/test/' or avm == '/test':
-		return 'HTTP/1.1 200 OK \n\n' + request
-	elif avm == '/media/' or avm == '/media/':
-		fls = ['<a href="'+i+'">'+i+'</a><br/>' for i in listdir('../files/')]
-		return 'HTTP/1.1 200 OK\n\n'+'\n'.join(fls)
-	elif avm[:7] == '/media/' and avm != '/media/':
-		file_path = avm[7:]
-		try:
-			with open('../files/'+file_path) as f:
-				return 'HTTP/1.1 200 OK\n\n'+f.read()
-		except Exception as e:
-			return 'HTTP/1.1 404 Not found\n\nFile not found'
+		agent = ' '.join(top[k].split()[1:]) 								#client name
+		return 'HTTP/1.1 200 OK\n\n Hello mister! \n You are: ' + agent 	#response 
+	elif avm=='/test/' or avm=='/test': 									#check for eq
+		return 'HTTP/1.1 200 OK \n\n' + request 							#returning request
+	elif avm=='/media/' or avm=='/media/': 									#check for eq
+		fls=['<a href="'+i+'">'+i+'</a><br/>' for i in listdir('../files/')]#list of files in /files/ with links
+		return 'HTTP/1.1 200 OK\n\n'+'\n'.join(fls) 						#returning response and list of files
+	elif avm[:7]=='/media/' and avm!='/media/': 							#check if it's smth in media
+		file_path=avm[7:] 													#file name
+		try: 
+			with open('../files/'+file_path) as f: 							#if it is file and it exists
+				return 'HTTP/1.1 200 OK\n\n'+f.read() 						#returning response & file 
+		except Exception as e: 												#if it is not file or file not exists
+			return 'HTTP/1.1 404 Not found\n\nFile not found' 
 		
 	else:
 		return 'HTTP/1.1 404 Not found\n\nPage not found'
@@ -34,19 +34,19 @@ def get_response(request):
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind(('localhost', 8000))  # Устанавливаем серверу локальный порт 8000
-server_socket.listen(0)  # Слушаем порты 
+server_socket.bind(('localhost', 8000))  									#set ro server local port 8000
+server_socket.listen(0)  													#listening ports 
 
 print 'Started'
 
 while 1:
 	try:
 		(client_socket, address) = server_socket.accept()
-		print 'Got new client', client_socket.getsockname()  # В случае появления нового клиента
-		request_string = client_socket.recv(2048)  # Полкчаем запрос от клиента
-		client_socket.send(get_response(request_string))  # Отправляем ответ
+		print 'Got new client', client_socket.getsockname()  				#if new client
+		request_string = client_socket.recv(2048)  							#getting reqest from client
+		client_socket.send(get_response(request_string))  					#sending answer
 		client_socket.close()
-	except KeyboardInterrupt:  # Обработка прерываний
+	except KeyboardInterrupt:  												#if ctrl + C
 		print 'Stopped'
-		server_socket.close()  # Останавливаем прослушку портов
+		server_socket.close()  												#stop listening
 		exit()
